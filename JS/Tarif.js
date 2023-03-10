c59907fe2642bd9d28f4f6d1a367cab84b094422
Bilel TOUATI <bilel83.touati@gmail.com>
2023-03-10 10:46:41 +0100

    Création et validation des premiers tests + Validation début de programme

--- JS/Tarif.js
+++ JS/Tarif.js
@@ -5,3 +5,27 @@
  */
 
 
+function RecupPoidsPrix(poids, prix){
+    const P1 = 250;
+    const P2 = 500;
+    const P3 = 750;
+    
+    if (poids <= P1){
+        return prix + 4.95;
+    }
+    else if (poids <= P2){
+        return prix + 6.70;
+    }
+    else if (revenus <= T3){
+        let impot = ((revenus - T2)*0.3)+((T2 - T1)*0.11);
+        return impot.toFixed(2);
+    }
+    else if (revenus <= T4){
+        let impot = ((revenus - T3)*0.41)+((T3 - T2)*0.3)+((T2 - T1)*0.11);
+        return impot.toFixed(2);
+    }
+    else if (revenus > T4){
+        let impot = ((revenus - T4)*0.45)+((T4 - T3)*0.41)+((T3 - T2)*0.3)+((T2 - T1)*0.11);
+        return impot.toFixed(2);
+    }
+}
--- Tests/TestsTarif.js
+++ Tests/TestsTarif.js
@@ -4,26 +4,42 @@
  */
 MesTestsUnitaires = TestCase('ImpotsTest');
 MesTestsUnitaires.prototype.testsPoids1 = function () {
-    assertEquals('0€ de revenus', 0, recupMontantImposition(0));
-    assertEquals('5000€ de revenus', 0, recupMontantImposition(5000));
-    assertEquals('10777€ de revenus', 0, recupMontantImposition(10777));
+    assertEquals('0€ de revenus', 5.95, RecupPoidsPrix(100, 1));
+    assertEquals('10778€ de revenus', 12.95, RecupPoidsPrix(200, 8));
+    assertEquals('10778€ de revenus', 28.95, RecupPoidsPrix(39, 24));
 };
 MesTestsUnitaires.prototype.testsPoids2 = function () {
-    assertEquals('10778€ de revenus', 0.11, recupMontantImposition(10778));
-    assertEquals('15000€ de revenus', 464.53, recupMontantImposition(15000));
-    assertEquals('27478€ de revenus', 1837.11, recupMontantImposition(27478));
+    assertEquals('10778€ de revenus', 6.70, RecupPoidsPrix(280, 0));
+    assertEquals('300g et le prix est de 3€', 9.70, RecupPoidsPrix(300, 3));
+    assertEquals('27478€ de revenus', 17.70, RecupPoidsPrix(498, 11));
 };
-MesTestsUnitaires.prototype.testsTranche3 = function () {
-    assertEquals('27479€ de revenus', 1837.41, recupMontantImposition(27479));
-    assertEquals('30000€ de revenus', 2593.71, recupMontantImposition(30000));
-    assertEquals('78570€ de revenus', 17164.71, recupMontantImposition(78570));
+MesTestsUnitaires.prototype.testsPoids3 = function () {
+    assertEquals('27479€ de revenus', 9.60, RecupPoidsPrix(550, 2));
+    assertEquals('30000€ de revenus', 34.60, RecupPoidsPrix(600, 25));
+    assertEquals('78570€ de revenus', 39.60, RecupPoidsPrix(704, 30));
 };
-MesTestsUnitaires.prototype.testsTranche4 = function () {
-    assertEquals('78571€ de revenus', 17165.12, recupMontantImposition(78571));
-    assertEquals('100000€ de revenus', 25951.01, recupMontantImposition(100000));
-    assertEquals('168994€ de revenus', 54238.55, recupMontantImposition(168994));
+MesTestsUnitaires.prototype.testsPoids4 = function () {
+    assertEquals('78571€ de revenus', 8.25, RecupPoidsPrix(78571));
+    assertEquals('100000€ de revenus', 25951.01, RecupPoidsPrix(100000));
+    assertEquals('168994€ de revenus', 54238.55, RecupPoidsPrix(168994));
 };
-MesTestsUnitaires.prototype.testsTranche5 = function () {
-    assertEquals('168995€ de revenus', 54239.00, recupMontantImposition(168995));
-    assertEquals('1000000€ de revenus', 428191.25, recupMontantImposition(1000000));
+MesTestsUnitaires.prototype.testsPoids5 = function () {
+    assertEquals('168995€ de revenus', 9.55, RecupPoidsPrix(168995));
+    assertEquals('1000000€ de revenus', 428191.25, RecupPoidsPrix(1000000));
+};
+MesTestsUnitaires.prototype.testsPoids6 = function () {
+    assertEquals('168995€ de revenus', 14.65, RecupPoidsPrix(168995));
+    assertEquals('1000000€ de revenus', 428191.25, RecupPoidsPrix(1000000));
+};
+MesTestsUnitaires.prototype.testsPoids7 = function () {
+    assertEquals('168995€ de revenus', 21.30, RecupPoidsPrix(168995));
+    assertEquals('1000000€ de revenus', 428191.25, RecupPoidsPrix(1000000));
+};
+MesTestsUnitaires.prototype.testsPoids8 = function () {
+    assertEquals('168995€ de revenus', 26.95, RecupPoidsPrix(168995));
+    assertEquals('1000000€ de revenus', 428191.25, RecupPoidsPrix(1000000));
+};
+MesTestsUnitaires.prototype.testsPoids9 = function () {
+    assertEquals('168995€ de revenus', 33.40, RecupPoidsPrix(168995));
+    assertEquals('1000000€ de revenus', 428191.25, RecupPoidsPrix(1000000));
 };
